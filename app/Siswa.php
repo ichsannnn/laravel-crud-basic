d9e06d79c88809a5a73c82ba9f46b288bf1ead82
Ichsan Firdaus <Ichsan Firdaus>
2016-05-04 01:32:57 +0700

    Add Siswa Model

--- app/Siswa.php
+++ app/Siswa.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Siswa extends Model
 {
-    //
+  use SoftDeletes;
+
+    protected $fillable = [
+      'nis', 'nama', 'kelas'
+    ];
 }
